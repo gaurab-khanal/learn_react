15ce450510d9c2a1da5279b0561174c47fc64d52
Gaurab Khanal <khanalgaurab98@gmail.com>
2023-09-08 16:49:30 +0545

    React Router

---
+++ 08reactRouter/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+
+function User() {
+
+    const {id} = useParams();
+
+  return (
+    <div className='bg-gray-700 text-white p-5'>User:{id} </div>
+  )
+}
+
+export default User
