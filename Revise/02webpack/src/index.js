4f77e24c4119b6bfd30f693913e6a62a2d7195f8
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-03-12 18:43:47 +0545

    Component and JSX

--- Revise/02webpack/src/index.js
+++ Revise/02webpack/src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const child1 = React.createElement("p", {}, "I am a child");
-const child2 = React.createElement("p", {}, "I am a child");
+// const child1 = React.createElement("p", {}, "I am a child");
+// const child2 = React.createElement("p", {}, "I am a child");
 
 // const div = React.createElement("div", { className: "text" }, [child1, child2]);
 
-const div = <div className="text">hello world</div>;
+const element = <div className="text">hello world</div>;
 
+const MyComponent = function () {
+  return (
+    <>
+      {element}
+      This is element
+    </>
+  );
+};
+
+console.log(MyComponent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(div);
+root.render(<MyComponent />);
