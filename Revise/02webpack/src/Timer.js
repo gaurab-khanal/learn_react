0f2c1f8bbf7ef8a2c87f252e6c1aeab67e2b1054
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-03-13 18:46:37 +0545

    basics about useLayoutEffect and its importance, some theories

--- Revise/02webpack/src/Timer.js
+++ Revise/02webpack/src/Timer.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 const Timer = ({ content }) => {
   const [counter, setCounter] = useState(0);
 
+  // this will run after browser is fully painted
   useEffect(() => {
     const time = setInterval(() => {
       console.log("I am running ");
       setCounter((prv) => prv + 1);
     }, 1000);
 
+    console.log("Inside useEffect");
     // clean up function will run for every unmounting of component
     // below is cleanup function which starts with return statement
 
@@ -22,6 +24,18 @@ const Timer = ({ content }) => {
       console.log("Content unmounted");
     };
   }, [content]);
+
+  // it will run before browser paints
+  // use this when you want to render something after calculating.
+  // so that the calculated value to showup in the first paint as
+  // useLayoutEffect will run before browser is painted.
+  // if you dont use this then the uncalculated value will render on the first paint
+
+  // it is used at a places where we want to calculate any element before the browser paints
+  // helps us avoid lags and gatters in our applications
+  useLayoutEffect(() => {
+    console.log("running uselayout effect");
+  }, []);
 
   return (
     <>
